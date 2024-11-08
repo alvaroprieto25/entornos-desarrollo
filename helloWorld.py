1f0d6ad96ad292fdb5a57e7d6cf4035255751688
Mateo Gómez-Plana Galiana <rawidea@github.com>
2024-11-08 17:40:07 +0100

    Print añadido y puntos y comas quitados

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,7 @@
 while True:
-	print("Hola mundo!");
-	print("Hola soy Fran");
-	print("Holasoyivan:D");
-	print("hola soy oscar");
-	print("Hola a todos, soy Naza");
-
+	print("Hola mundo!")
+	print("Hola soy Fran")
+	print("Holasoyivan:D")
+	print("hola soy oscar")
+	print("Hola a todos, soy Naza")
+    print("Hola, soy Mateo")
